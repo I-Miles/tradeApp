12370b1b89dd26ad8b8e50bddc7144888a644e72
Felipe <142539179+Pereirinha1@users.noreply.github.com>
2024-08-20 15:10:28 -0300

    Update App.js

--- App.js
+++ App.js
@@ -7,22 +7,26 @@ import RegisterScreen from './screens/RegisterScreen';
 import HomeScreen from './screens/HomeScreen';
 import LoginOnnScreen from './screens/LoginOnnScreen';
 import CheckoutScreen from './screens/CheckoutScreen';
-import RegisterproScreen from './screens/RegisterproScreen';
-import PurchaseConfirmationScreen from './screens/PurchaseConfirmationScreen'
+import PurchaseConfirmationScreen from './screens/PurchaseConfirmationScreen';
+import RegisterProScreen from './screens/RegisterProScreen';
+import RegisterForn from './screens/RegisterForn';
+import ListTable from './screens/ListTable';
 
 const Stack = createStackNavigator();
 
 const App = () => {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="Home">
+      <Stack.Navigator initialRouteName="Login">
+        <Stack.Screen name="List" component={ListTable} />
+        <Stack.Screen name="forn" component={RegisterForn} />
         <Stack.Screen name="Login" component={LoginScreen} />
         <Stack.Screen name="LoOn" component={LoginOnnScreen} />
         <Stack.Screen name='Register' component={RegisterScreen} />
         <Stack.Screen name="Home" component={HomeScreen} />
         <Stack.Screen name="Checkout" component={CheckoutScreen} />
-        <Stack.Screen name="ProForn" component={RegisterproScreen} />
         <Stack.Screen name="Confirmation" component={PurchaseConfirmationScreen} />
+        <Stack.Screen name='ProForn' component={RegisterProScreen} />
       </Stack.Navigator>
     </NavigationContainer>
   );
